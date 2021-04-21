3824aa34948fd5d46bd98299fef3a5e956d52302
Sebastian <sebastian.gralikowski@dunelm.com>
2021-04-21 22:45:13 +0100

    update colors

--- src/components/theme/index.tsx
+++ src/components/theme/index.tsx
@@ -1,7 +1,7 @@
 export { default as GlobalStyle } from './globalStyle'
 
 export type Size = 'xs' | 's' | 'm' | 'l' | 'xl'
-type Colors = 'primary' | 'secondaryLight' | 'secondaryDark' | 'white'
+type Colors = 'primaryDark' | 'primaryLight' | 'secondary' | 'grey' | 'white'
 
 export const size: Record<Size, string> = {
     xs: '1.4rem',
@@ -16,9 +16,10 @@ export const padding: Record<Size, string> = size
 
 
 export const colors: Record<Colors, string> = {
-    primary: '#5EBDC7',
-    secondaryLight: '#353A4E',
-    secondaryDark: '#2F3343',
+    primaryDark: '#AAAAAA',
+    primaryLight: '#E0EBED',
+    secondary: '#171B20',
+    grey: '#A1A1A1',
     white: '#fff'
 }
 
